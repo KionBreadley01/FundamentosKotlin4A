5e92b90b65fa53e1b3d5c46749f1b6e0c573631d
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-09-14 10:02:28 -0600

    comoensando con kotlin

---
+++ helo.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Hello wordl")
+    Println("Hola mundo")
+    print("mundo")
+    println()
+    println("Mis alumnos son" + 10)
+
+}
---
+++ src/main/kotlin/hello.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Hello wordl")
+    println() rint("Hola mundo")
+    print("mundo")
+    println()
+    println("Mis alumnos son" + 10)
+
+}
